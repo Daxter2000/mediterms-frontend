1386f837aa2ea13c7e7cb21fbbe16001fa206088
adrian <adrianperezm.00@gmail.com>
2023-01-15 21:22:33 -0700

    gh pages

--- src/config/axiosConfig.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-import { hostURL } from "scripts/generalVariables"
-
-axios.defaults.baseURL = hostURL
-axios.defaults.withCredentials = true
-axios.defaults.headers.common['Authorization'] = `Bearer ${global.getCookieToken()}`
--- src/hooks/useSideMenuContext.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react"
-import SideMenuContext from "context/SideMenuContext"
-
-const useSideMenuContext = () => useContext(SideMenuContext)
-
-export default useSideMenuContext
